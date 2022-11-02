8f8111d485d5b6ef0b785256856ccd2b035b967d
AbeeraTahir <abeeratahir2018@gmail.com>
2022-11-02 22:11:14 +0500

    Implement add task functionality

--- src/index.js
+++ src/index.js
@@ -1,37 +1,42 @@
 import './style.css';
 
+const form = document.getElementById('add-to-list');
 const listItems = document.querySelector('.to-do-list');
-const toDoList = [
-  {
-    index: 0,
-    completed: 'true',
-    description: 'wash the dishes',
-  },
-  {
-    index: 2,
-    completed: 'true',
-    description: 'cook food',
-  },
-  {
-    index: 1,
-    completed: 'true',
-    description: 'complete project',
-  },
-];
+const taskArr = [];
 
-const showListItems = () => {
-  toDoList.sort((a, b) => a.index - b.index);
-  toDoList.forEach((item) => {
-    const listItem = `
-    <li>
-      <div class="check">
-        <input type="checkbox" name="${item.index}" id=${item.index}>
-        <p>${item.description}</p>
-      </div>
-      <i class="fa-solid fa-ellipsis-vertical"></i>
-    </li>`;
-    listItems.insertAdjacentHTML('beforeend', listItem);
-  });
+// function for display added task to list
+const displayTask = (task) => {
+  const listItem = `
+  <li>
+    <div class="check">
+      <input type="checkbox" name="${task.description}">
+      <p>${task.description}</p>
+    </div>
+    <i class="fa-solid fa-ellipsis-vertical"></i>
+  </li>`;
+  listItems.insertAdjacentHTML('beforeend', listItem);
 };
 
-showListItems();
+// function for adding task to list
+const addTask = (task) => {
+  const taskObj = {};
+  taskObj.index = taskArr.length + 1;
+  taskObj.description = task;
+  taskObj.completed = false;
+  displayTask(taskObj);
+  taskArr.push(taskObj);
+  localStorage.setItem('tasks', JSON.stringify(taskArr));
+};
+
+// displaying tasks on window loading
+window.addEventListener('DOMContentLoaded', () => {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  tasks.forEach((task) => displayTask(task));
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const task = document.getElementById('task');
+  addTask(task.value);
+  task.value = '';
+});
