7fce138a1edfe7c15b3ba03702afefb1412b8dae
AbeeraTahir <abeeratahir2018@gmail.com>
2022-12-28 20:32:20 +0500

    Fix errors

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const addTask = (task) => {
 // function to render all tasks
 const renderTasks = () => {
   const tasks = getLocalStorage();
-  tasks.map((task) => addTaskUI(task));
+  tasks.map((task) => addTask(task));
 };
 
 // displaying tasks on window loading
